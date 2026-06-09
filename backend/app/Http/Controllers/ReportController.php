cd444fb0d38cfbdbd70e1da8b11d56b8de055860
Darshan Baniya <dbaniya@myune.edu.au>
2026-06-09 12:46:05 +1000

    Enable API routing and add reports endpoint

--- backend/app/Http/Controllers/ReportController.php
+++ backend/app/Http/Controllers/ReportController.php
@@ -4,7 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// Controller to return a JSON response with a list of reports
 class ReportController extends Controller
 {
-    //
+    public function index()
+    {
+        return response() ->json([ 
+            'reports' => [
+                [
+                    'name' =>'Sales Dashboard',
+                    'report_id' => '1',
+                    'embed_url' => 'https://app.powerbi.com/'
+                    
+                ]
+            ]
+        ]);  
+    }  
+
 }
